a0bd5199ff548979d56913fa45d33289e1f56f90
algoritmico67 <mikelbalboa@icloud.com>
2025-09-29 19:49:18 -0600

    Add files via upload

--- 1.py
+++ 1.py
@@ -10,6 +10,34 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        self.image = transform.scale(imge.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self_speed
+    def updae_l(self):
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self_speed
+
+ 
+
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
